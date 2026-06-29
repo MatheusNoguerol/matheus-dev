9210d77dd74590b910a41d3d9a093c6dcbd18d7d
Matheus Noguerol <matheus.noguerol@totalcontrol.com.br>
2026-06-29 20:37:32 -0300

    Logic height removed in App.vue .

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,22 @@
     <div class="min-h-screen bg-white dark:bg-gray-900">
         <Header />
 
-        <main :style="containerStyle">
+        <main>
             <HeroSection />
 
             <About />
 
             <Projects />
+
+            <Contact />
         </main>
     </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted, onUnmounted } from 'vue';
 import Header from './components/layout/Header.vue';
 import HeroSection from './components/sections/HeroSection.vue';
 import About from './components/sections/About.vue';
 import Projects from './components/sections/Projects.vue';
-
-const heightPadding = ref(96);
-
-const calculatePadding = () => {
-    const alturaTela = window.innerHeight;
-    heightPadding.value = alturaTela > 844 ? 150 : 96;
-};
-
-const containerStyle = computed(() => {
-    return heightPadding.value === 150 ? { paddingBottom: heightPadding.value + 'px' } : {};
-});
-
-onMounted(() => {
-    calculatePadding();
-    window.addEventListener('resize', calculatePadding);
-});
-
-onUnmounted(() => {
-    window.removeEventListener('resize', calculatePadding);
-});
+import Contact from './components/sections/Contact.vue';
 </script>
